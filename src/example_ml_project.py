d5172b93222e6ee0ea3c44234177ba0fa6acf5fc
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-03-06 12:30:03 +0000

    Modify code to make it easier to add more instances to an existing dataset.

--- src/example_ml_project.py
+++ src/example_ml_project.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from ml_resources import ( create_dataset, 
+from ml_resources import ( update_dataset,
     read_dataset_from_file, 
     create_model_data_object, 
     read_dataset_from_file,
@@ -7,9 +7,10 @@ from ml_resources import ( create_dataset,
     calculate_accuracy,
     create_embedding_from_item,
     save_versioned_pickle_file,
-    create_text_embeddings
+    create_embedding_from_item
 )
 from ml_resources.data import colectica_utility
+import pandas as pd
 
 colectica_client = colectica_utility.C
 all_studies = colectica_client.search_items(colectica_client.item_code('Series'), SearchLatestVersion=True)['Results']
@@ -59,32 +60,31 @@ all_question_embeddings=read_dataset_from_file('../data/lha_usoc_question_embedd
 # The code below shows how we get the model data for uk.lha, uk.user.ukhls and those two
 # studies combined, create and train a prediction model, and measure that model's accuracy...
 
-identifiers_lha_usoc = list(item_topics['uk.lha'].keys()) + list(item_topics['uk.iser.ukhls'].keys())
 identifiers_usoc = list(item_topics['uk.iser.ukhls'].keys())
 identifiers_lha = list(item_topics['uk.lha'].keys())
-dataset_lha_usoc = create_dataset(identifiers, 
-    all_question_embeddings['uk.lha'] | all_question_embeddings['uk.iser.ukhls'], 
-    'QuestionEmbedding', 
-    'Topic', 
-    item_topics['uk.lha'] | item_topics['uk.iser.ukhls'])
 
-dataset_lha = create_dataset(identifier_lha, 
+dataset = update_dataset('uk.lha',
+    identifiers_lha,
     all_question_embeddings['uk.lha'], 
     'QuestionEmbedding', 
     'Topic', 
     item_topics['uk.lha'])
 
-dataset_usoc = create_dataset(identifiers_usoc, 
+dataset = update_dataset('uk.iser.ukhls',
+    identifiers_usoc, 
     all_question_embeddings['uk.iser.ukhls'], 
     'QuestionEmbedding', 
     'Topic',
-    item_topics['uk.iser.ukhls'])
+    item_topics['uk.iser.ukhls'],
+    dataset)
 
-X_train, X_test, y_train, y_test = train_test_split(dataset_lha_usoc['InputFeatures'],
-    dataset_lha_usoc['Targets'], train_size=0.9)
-X_train, X_test, y_train, y_test = train_test_split(dataset_lha['InputFeatures'],
+X_train, X_test, y_train, y_test = train_test_split(
+    pd.concat([dataset['uk.lha']['InputFeatures'], dataset['uk.iser.ukhls']['InputFeatures']]),
+    pd.concat([dataset['uk.lha']['Targets'], dataset['uk.iser.ukhls']['Targets']]),
+    train_size=0.9)
+X_train, X_test, y_train, y_test = train_test_split(dataset['uk.lha']['InputFeatures'],
     dataset_lha['Targets'], train_size=0.9 )    
-X_train, X_test, y_train, y_test = train_test_split(dataset_usoc['InputFeatures'],
+X_train, X_test, y_train, y_test = train_test_split(dataset['uk.iser.ukhls']['InputFeatures'],
     dataset_usoc['Targets'], train_size=0.9 )    
 
 lr_model_data=create_model_data_object(X_train, X_test, y_train, y_test)
--- src/ml_resources/data/make_dataset.py
+++ src/ml_resources/data/make_dataset.py
@@ -2,18 +2,25 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle
 
-def create_dataset(identifiers, 
+def update_dataset(study_agency_id,
+    identifiers,
     input_features,
     input_feature_name,
     target_name,
-    targets):
-    X=pd.DataFrame({}, columns=[input_feature_name])
-    y=pd.DataFrame({}, columns=[target_name])
+    targets,
+    dataset={}):
+    """Updates an existing dataset specified in the dataset keyword input argument, or 
+    creates a new dataset if an existing dataset is not specified."""
+    if study_agency_id not in dataset:
+        X=pd.DataFrame({}, columns=[input_feature_name])
+        y=pd.DataFrame({}, columns=[target_name])
+        dataset[study_agency_id]={"InputFeatures": X, "Targets": y}
     for identifier in identifiers:
-        X.loc[identifier] = [input_features[identifier]]
-        y.loc[identifier] = [targets[identifier]]
-    return { "InputFeatures": X, "Targets": y}
-    
+        if identifier not in dataset[study_agency_id].keys():
+            dataset[study_agency_id]["InputFeatures"].loc[identifier] = [input_features[identifier]]
+            dataset[study_agency_id]["Targets"].loc[identifier] = [targets[identifier]]
+    return dataset
+
 def add_input_feature_to_dataset(identifiers, 
     input_features, 
     new_input_feature_name, 
