526edfa6a6f0d1e29333ed0a62bdacd56d7ef487
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-03-09 12:21:17 +0000

    add projects folder

---
+++ projects/am1_project/example_ml_project.py
@@ -0,0 +1,170 @@
+from sklearn.model_selection import train_test_split
+from ml_resources import ( update_dataset,
+    add_input_feature_to_dataset,
+    read_dataset_from_file, 
+    create_model_data_object, 
+    read_dataset_from_file,
+    train_model,
+    calculate_accuracy,
+    create_embedding_from_item,
+    save_versioned_pickle_file,
+    create_embedding_from_item
+)
+from ml_resources.data import colectica_utility
+import pandas as pd
+
+colectica_client = colectica_utility.C
+all_studies = colectica_client.search_items(colectica_client.item_code('Series'), SearchLatestVersion=True)['Results']
+all_question_summaries={}
+all_variable_labels={}
+# Get question summaries for one study...
+colectica_utility.get_questions_for_studies(all_studies[0], all_question_summaries)
+
+# Get question summaries for all studies...
+colectica_utility.get_questions_for_studies(all_studies, all_question_summaries)
+
+all_question_embeddings={}
+for agencyId in all_question_summaries.keys():
+    print(f"Creating question embeddings for {agencyId}...")
+    for question_summary in all_question_summaries[agencyId].items():
+        create_embedding_from_item(agencyId, question_summary[0], question_summary[1], all_question_embeddings)
+
+# Show that we have an equal number of question summaries and question embeddings for all
+# studies, i.e. we have embeddings for all summaries...
+for agencyId in all_question_summaries.keys():
+    print(f"{agencyId}, {len(list(all_question_embeddings[agencyId].items()))}, {len(list(all_question_summaries[agencyId].items()))}")
+
+item_topics={}
+for study in [all_studies[0]]:
+    study_search_set = [{
+                 "agencyId": study['AgencyId'],
+                 "identifier": study['Identifier'],
+                 "version": study['Version']
+                }]
+    print(f"Getting topics for items in {study['AgencyId']}...")
+    study_question_items = colectica_client.search_items(colectica_client.item_code('Question'),
+        SearchSets=study_search_set,
+        SearchLatestVersion=True)['Results']
+    get_topics_for_items(study_question_items, colectica_client, topics=item_topics)
+
+# Assuming that we got question summaries for the uk.lha and uk.iser.ukhls studies and 
+# calculated their embeddings, and obtained the topics assigned to those questions, here 
+# is how we would save them into a versioned pickle file... 
+save_versioned_pickle_file(all_question_embeddings, 'lha_usoc_question_embeddings', folder='../data')
+save_versioned_pickle_file(item_topics, 'lha_usoc_topics', folder='../data')
+
+# Assuming that we have already saved question embeddings and their associated topics into 
+# pickle files, here is how we read them from the pickle files...
+item_topics=read_dataset_from_file('../data/lha_usoc_topics_1.pickle')
+all_question_embeddings=read_dataset_from_file('../data/lha_usoc_question_embeddings_1.pickle')
+
+# The code below shows how we get the model data for uk.lha, uk.user.ukhls and those two
+# studies combined, create and train a prediction model, and measure that model's accuracy...
+
+identifiers_usoc = list(item_topics['uk.iser.ukhls'].keys())
+identifiers_lha = list(item_topics['uk.lha'].keys())
+
+# Create a dataset...
+
+dataset = update_dataset('uk.lha',
+    identifiers_lha,
+    all_question_embeddings['uk.lha'], 
+    'QuestionEmbedding', 
+    'Topic', 
+    item_topics['uk.lha'])
+
+# ...and now we update the dataset we just created so it includes data from another study...
+
+dataset = update_dataset('uk.iser.ukhls',
+    identifiers_usoc, 
+    all_question_embeddings['uk.iser.ukhls'], 
+    'QuestionEmbedding', 
+    'Topic',
+    item_topics['uk.iser.ukhls'])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    pd.concat([dataset['uk.lha']['InputFeatures'], dataset['uk.iser.ukhls']['InputFeatures']]),
+    pd.concat([dataset['uk.lha']['Targets'], dataset['uk.iser.ukhls']['Targets']]),
+    train_size=0.9)
+X_train, X_test, y_train, y_test = train_test_split(dataset['uk.lha']['InputFeatures'],
+    dataset['uk.lha']['Targets'], train_size=0.9 )
+X_train, X_test, y_train, y_test = train_test_split(dataset['uk.iser.ukhls']['InputFeatures'],
+    dataset['uk.iser.ukhls']['Targets'], train_size=0.9 )
+
+lr_model_data=create_model_data_object(X_train, X_test, y_train, y_test)
+trainedModel=train_model(lr_model_data, selected_input_features='QuestionEmbedding')
+trainedModel.predict(list(lr_model_data['X_test']['QuestionEmbedding'].values))
+predictions_with_probabilities=trainedModel.predict_proba(list(lr_model_data['X_test']['QuestionEmbedding'].values))
+wrong_predictions=calculate_accuracy(trainedModel,
+    predictions_with_probabilities, 
+    lr_model_data['X_test']['QuestionEmbedding'].values, 
+    lr_model_data['y_test'].values,
+    N=3)
+
+all_question_categories=colectica_utility.get_categories_for_questions('uk.iser.ukhls', identifiers_usoc)
+save_versioned_pickle_file(all_question_categories, 'all_question_categories', folder='../data')
+
+# If we have already saved the question categories to a pickle file, we can read them from that...
+
+all_question_categories=read_dataset_from_file('../data/all_question_categories_1.pickle')
+
+all_question_category_embeddings={}
+for agencyId in all_question_categories.keys():
+    print(f"Creating question category embeddings for {agencyId}...")
+    for question_categories in all_question_categories[agencyId].items():
+        create_embedding_from_item(agencyId,
+            question_categories[0],
+            question_categories[1],
+            all_question_category_embeddings)
+
+save_versioned_pickle_file(all_question_category_embeddings, 'all_question_category_embeddings', folder='../data')
+
+# If we have already saved the question categories embeddings to a pickle file, we can
+# read them from that...
+
+all_question_category_embeddings=read_dataset_from_file('../data/all_question_category_embeddings_1.pickle')
+
+add_input_feature_to_dataset('uk.iser.ukhls',
+    identifiers_usoc,
+    all_question_category_embeddings,
+    "QuestionCategoryEmbeddings",
+    dataset)
+
+save_versioned_pickle_file(dataset, 'questions_dataset', folder='../data')
+
+# Here is how we add the original text values for summaries and categories to
+# the dataset. We can use these to filter out row where these values have
+# certain values (e.g. they are empty, they are "yes/no")
+add_input_feature_to_dataset('uk.iser.ukhls',
+    identifiers_usoc,
+    all_question_summaries,
+    "QuestionSummaries",
+    dataset)
+
+add_input_feature_to_dataset('uk.iser.ukhls',
+    identifiers_usoc,
+    all_question_categories,
+    "QuestionCategories",
+    dataset)
+
+save_versioned_pickle_file(dataset, 'usoc_questions_summaries_and_categories_dataset', folder='../data')
+dataset=read_dataset_from_file('../data/usoc_questions_summaries_and_categories_dataset_1.pickle')
+dataset_transformed_categories=transform_input_feature_categories(dataset, 'uk.iser.ukhls', 'QuestionCategories')
+FINE BUT NOW WITH THE ABOVE YOU NEED TO CALCULATE THE EMBEDDINGS?
+YOU NEED TO HAVE A STRATEGY WHERE YOU CAN IMPUTE/CHANGE COLUMNS, DROP ROWS OR COLUMNS,
+EG PERHAPS IF GREATER THAN X% ARE BLANK/INVALID, ALL THIS NEEDS TO BE DONE BEFORE YOU
+START CALCULATING EMBEDDINGS, SO THIS TRANSFORMATION STEP NEEDS TO BE MOVED UP TO THE TOP
+
+PROCESS: 
+1. GET DATA FROM COLECTICA
+2. PERFORM QUALITY CONTROL, E.G. REMOVE DATA WITH MISSING, INADEQUATE values, ARRAYS with
+PARTICULAR VALUES
+3. PERFORM DATA TRANSFORMATIONS EXCLUDING CALCULATION OF EMBEDDINGS EG CONVERT ARRAYS
+TO ORDERED LOWER CASE STRING
+4. TRANSFORM TEXT COLUMNS TO EMBEDDINGS
+5. CREATE DATASET WITH PARTICULAR SET OF COLUMNS EG DROP THE RAW TEXT THAT WE HAVE created
+EMBEDDINGS FROM
+
+X_train, X_test, y_train, y_test = train_test_split(dataset_transformed_categories['uk.iser.ukhls']['InputFeatures'],
+    dataset_transformed_categories['uk.iser.ukhls']['Targets'], train_size=0.9 )
+X_train[X_train['QuestionCategories'].str.len()==0]
---
+++ projects/am1_project/get_data_from_colectica.py
@@ -0,0 +1,26 @@
+from ml_resources.data import colectica_utility
+from ml_resources import save_versioned_pickle_file
+colectica_client = colectica_utility.C
+
+# We get all the studies...
+all_studies = colectica_client.search_items(colectica_client.item_code('Series'), SearchLatestVersion=True)['Results']
+am1_data={}
+# We get the questions for the first study (should be usoc)
+colectica_utility.get_questions_for_studies(all_studies[0], am1_data, "Summary")
+save_versioned_pickle_file(usoc_question_summaries, 'usoc_question_summaries', folder='../data')
+
+# We get the topics for questions in the usoc study...
+item_topics={}
+for study in [all_studies[0]]:
+    print(f"Getting topics for items in {study['AgencyId']}...")
+    colectica_utility.get_topics_for_items(list(am1_data[study['AgencyId']].keys())[0:5], 
+        study['AgencyId'],
+        colectica_client.item_code('Question Group'), 
+        colectica_client, 
+        topics=am1_data,
+        verbose=True)
+
+# Assuming that we got the topics for usoc questions, here is how we would save them into a 
+# versioned pickle file... 
+save_versioned_pickle_file(item_topics, 'am1_data', folder='../data')
+
