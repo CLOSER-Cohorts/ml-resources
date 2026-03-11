ccd59e02353b05de17d54a69b7b9db4009a97a1d
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-03-11 14:31:44 +0000

    remove comment

--- projects/am1_project/get_data_from_colectica.py
+++ projects/am1_project/get_data_from_colectica.py
@@ -2,7 +2,6 @@ from ml_resources.data import colectica_utility
 from ml_resources import read_dataset_from_file, save_versioned_pickle_file
 colectica_client = colectica_utility.C
 
-
 study_index=3
 # We get all the studies...
 all_studies = colectica_client.search_items(colectica_client.item_code('Series'), SearchLatestVersion=True)['Results']
@@ -30,10 +29,6 @@ for study_agency_id in am1_data.keys():
     print(f"Getting categories for questions in {study_agency_id}...")
     colectica_utility.get_categories_for_questions(study_agency_id, list(am1_data[study_agency_id].keys()),
         am1_data, verbose=True)
-
-    #colectica_utility.get_categories_for_questions(study['AgencyId'], list(am1_data[study['AgencyId']].keys())[0:10],
-    #    am1_data, verbose=True)
-
 
 # Assuming that we got the topics for usoc questions, here is how we would save them into a 
 # versioned pickle file... 
