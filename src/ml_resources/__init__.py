cc728e393d1701fe74698adc35bc7a06a98470fa
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-03-27 16:56:48 +0000

    add files I forgot to add in pull request

--- src/ml_resources/__init__.py
+++ src/ml_resources/__init__.py
@@ -3,5 +3,6 @@ from .data.pickle_utility import *
 from .data.text_embedding_pipeline import *
 from .features.create_text_embeddings import create_embedding_from_item
 from .models.train_model import train_model
-from .models.predict_model import calculate_accuracy
+from .models.predict_model import calculate_accuracy, obtain_correctly_labelled_data
 from .models.utility import create_model_package
+from .data.colectica_utility import obtain_items_from_colectica
