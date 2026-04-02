e57531e1b1b1bf481560cc60a9bd689f16d2ed57
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-02 16:44:57 +0100

    wip

--- projects/am2_project/src/check_for_data.py
+++ projects/am2_project/src/check_for_data.py
@@ -5,6 +5,9 @@ import json
 from datetime import datetime
 from .data.utility import (
         check_for_newly_available_data)
+from src.ml_resources import (
+    save_versioned_pickle_file
+    )
 from src.slack.utility import send_message_to_slack
 
 def setup_logging():
@@ -25,8 +28,16 @@ def main(args):
         logging.info(f"Running task with param: {args.param}")
         with open("./projects/am2_project/config/am2_config.json") as f:
             project_config = json.load(f)
-        check_for_newly_available_data(project_config)
-
+        results = check_for_newly_available_data(project_config)
+        if len(results['new_item_urns'])>0:
+            save_versioned_pickle_file(
+                results['all_am2_relationships_data'],
+                'am2_relationships_data_for_future_model',
+                folder='./projects/am2_project/data/pending_training_data',
+                )
+            # need to properly import the model below in future issue    
+            #model.predict(results['all_am2_relationships_data'])
+            
         # Example task
         print(f"Hello at {datetime.now()}")
 
