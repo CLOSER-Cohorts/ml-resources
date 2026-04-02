28a114f40fffa782b521ab5eecc1fff40fd58f4b
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-02 21:11:14 +0100

    add code that sends anomaly predictions to the slack channel

--- projects/am2_project/src/check_for_data.py
+++ projects/am2_project/src/check_for_data.py
@@ -70,11 +70,15 @@ def main(args):
                     order=list(all_item_models[item_type]['model'].feature_names_in_)
                     new_am2_relationships_data_single_type=new_am2_relationships_data_single_type.reindex(
                         columns=order).replace({np.nan: 0}) 
-                    a=all_item_models[item_type]['model'].predict(
+                    predictions=all_item_models[item_type]['model'].predict(
                         new_am2_relationships_data_single_type
                         )
-                    print("PREDICTION")
-                    print(a)
+                    indices_flagged = [i for i, x in enumerate(predictions) if x == 1]    
+                    anomalies=list(new_am2_relationships_data_single_type.index[indices_flagged])
+                    if len(anomalies)>0:
+                        print("ANOMALIES PREDICTED")
+                        print(anomalies)
+                        send_message_to_slack(list)
                 all_new_am2_relationships_data[item_type]=new_am2_relationships_data_single_type
             save_versioned_pickle_file(
                 all_new_am2_relationships_data,
