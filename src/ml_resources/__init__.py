be98dd7da81dc9922c8b6029ac492947833d9306
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-07 18:17:54 +0100

    create initial version of api for AM1

--- src/ml_resources/__init__.py
+++ src/ml_resources/__init__.py
@@ -1,6 +1,6 @@
 from .data.make_dataset import *
 from .data.pickle_utility import *
-#from .data.text_embedding_pipeline import *
+from .data.text_embedding_pipeline import *
 #from .features.create_text_embeddings import create_embedding_from_item
 from .models.train_model import train_model
 from .models.predict_model import calculate_accuracy, obtain_correctly_labelled_data
