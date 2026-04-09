68fc6de839f750cb24333eb758aae74ad41f3ead
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-09 13:45:52 +0100

    modify am1 model so it can include variables

--- projects/am1_project/get_data_from_colectica.py
+++ projects/am1_project/get_data_from_colectica.py
@@ -3,24 +3,32 @@ from src.ml_resources import read_dataset_from_file, save_versioned_pickle_file
 import json
 colectica_client = colectica_utility.C
 
-with open("./config/am1_config.json") as f:
+with open("projects/am1_project/config/am1_config.json") as f:
     project_config = json.load(f)
 
 # If you're starting from scratch, create the am1_data object...
 am1_data={}
 # ...otherwise read it in from a pickle file.
-am1_data=read_dataset_from_file('../projects/am1_project/data/am1_data_3.pickle')
+am1_data=read_dataset_from_file('../projects/am1_project/data/am1_data/am1_data_2.pickle')
 
 # We get the questions for the usoc study...
-colectica_utility.get_questions_in_containing_items(project_config['studies'], am1_data, "Summary")
+colectica_utility.get_items_in_containing_items(project_config['Sweeps'],
+    am1_data,
+    "Summary",
+    colectica_client.item_code('Question'))
+
+colectica_utility.get_items_in_containing_items(project_config['Sweeps'],
+    am1_data,
+    "Label",
+    colectica_client.item_code('Variable'))
 
 # We get the topics for questions in the usoc study...
-for study in project_config['studies']:
+for study in project_config['Sweeps']:
     print(f"Getting topics for items in {study['AgencyId']}...")
     if study['AgencyId'] in am1_data.keys():
         colectica_utility.get_topics_for_items(list(am1_data[study['AgencyId']].keys()),
             study['AgencyId'],
-            colectica_client.item_code('Question Group'),
+            [colectica_client.item_code('Variable Group'), colectica_client.item_code('Question Group')],
             colectica_client,
             topics=am1_data,
             verbose=True)
@@ -28,8 +36,8 @@ for study in project_config['studies']:
 # We get the categories for questions in our dataset (from all studies)...
 
 for study_agency_id in am1_data.keys():
-    print(f"Getting categories for questions in {study_agency_id}...")
-    colectica_utility.get_categories_for_questions(study_agency_id,
+    print(f"Getting categories for items in {study_agency_id}...")
+    colectica_utility.get_categories_for_items(study_agency_id,
         list(am1_data[study_agency_id].keys()),
         all_items=am1_data,
         verbose=True)
