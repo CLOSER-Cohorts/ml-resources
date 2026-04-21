5021f0ffd255419443421dbf51671c184e44804d
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-21 17:06:47 +0100

    created an am1 model trained on all studies

--- src/ml_resources/data/text_embedding_pipeline.py
+++ src/ml_resources/data/text_embedding_pipeline.py
@@ -25,6 +25,7 @@ def apply_pipeline(data, columns):
     transformed_embeddings = pd.DataFrame({"summary_embeddings": [x[0:embedding_length] for x in transformed_data],
      "category_embeddings": [x[embedding_length:embedding_length*2] for x in transformed_data],
      "item_type": [x[embedding_length*2] for x in transformed_data],
-     "topic": [x[embedding_length*2+1] for x in transformed_data]}
+     "topic": [x[embedding_length*2+1] for x in transformed_data],
+     "has_categories": [x[embedding_length*2+2] for x in transformed_data]}
      )
     return transformed_embeddings
