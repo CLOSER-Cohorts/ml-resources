3726ac0bb1a552da9cd95aac140da67dd0e1572f
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-04-24 16:08:36 +0100

    add monitoring to am1

--- projects/am1_project/api/main.py
+++ projects/am1_project/api/main.py
@@ -5,8 +5,13 @@ from pydantic import BaseModel
 import pickle
 import numpy as np
 import pandas as pd
+import time
+import logging
 from src.ml_resources import (
     apply_pipeline)
+from src.logging.utility import StructuredMessage, setup_logging
+
+logger=setup_logging(project="am1_project", log_file="logs/am1_log.json")
 
 #from src.ml_resources import read_dataset_from_fil
 # v
@@ -14,6 +19,7 @@ from src.ml_resources import (
 
 modelfile = open('./projects/am1_project/model/trainedModelAllStudies/trainedModelAllStudies_1.pickle', 'rb')
 trainedModel = pickle.load(modelfile)
+categories=trainedModel.classes_.tolist()
 """
 testDataFile = open('./projects/am1_project/data/testData/testData_1.pickle', 'rb')
 testData = pickle.load(testDataFile)
@@ -52,6 +58,7 @@ async def create_item(item: Item):
 """
 @app.post("/categorise_questions/")
 async def categorise_questions(item: Item):
+    start_time = time.time()
     df = pd.DataFrame(item.dict())
     transformed_embeddings = apply_pipeline(df, ['TextLabel', 'ItemCategories'])
     X = np.hstack([
@@ -60,9 +67,31 @@ async def categorise_questions(item: Item):
      np.vstack(transformed_embeddings['item_type']),
      np.vstack(transformed_embeddings['has_categories'])
     ])
-    result = trainedModel.predict(X)
+    #result = trainedModel.predict(X)
+    results=trainedModel.predict_proba(X)
+    predictions=[]
+    confidence_scores=[]
+    for result in results:
+        #result.tolist().index(max(result))
+        confidence_scores.append(max(result).item())
+        predictions.append(categories[result.tolist().index(max(result))])
+    latency = time.time() - start_time
     print(result)
-    return list(result)
+    print(transformed_embeddings['item_type'])
+    logger.info(StructuredMessage(message='Categorise text',
+        application="am1",
+        operation_type="item_classification",
+        number_of_predictions=len(transformed_embeddings),
+        labels=df['TextLabel'].values.tolist(),
+        size_of_labels=df['TextLabel'].memory_usage(deep=True),
+        categories=df['ItemCategories'].values.tolist(),
+        size_of_categories=df['ItemCategories'].memory_usage(deep=True),
+        item_types=df['ItemType'].values.tolist(),
+        latency=latency,
+        results=predictions,
+        confidence=confidence_scores
+        ))
+    return list(predictions)
     
 
     
