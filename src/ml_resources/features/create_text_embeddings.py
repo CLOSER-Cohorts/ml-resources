6beaa5aa58db9ad0fe937379b01d547eaae44b31
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-05-10 16:52:43 +0100

    fix bugs in code that checks for new am2 data

--- src/ml_resources/features/create_text_embeddings.py
+++ src/ml_resources/features/create_text_embeddings.py
@@ -1,3 +1,4 @@
+"""
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('all-MiniLM-L6-v2')
 from sklearn.pipeline import Pipeline
@@ -10,7 +11,7 @@ def create_embedding_from_item(agencyId, identifier, item_text, item_embeddings=
     if agencyId not in item_embeddings.keys():
             item_embeddings[agencyId] = {}
     item_embeddings[agencyId][identifier] = model.encode(item_text)
-
+"""
 # Below is code that creates a pipeline object for creating embeddings 
 """
 question_summaries=read_dataset_from_file('../data/usoc_summaries_1.pickle')
