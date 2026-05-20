963b4995d838f7914869e84b5dd1cb620e0ddafd
o.lyttleton@ucl.ac.uk <o.lyttleton@ucl.ac.uk>
2026-05-20 16:53:46 +0100

    define inference contract for am1

--- src/ml_resources/data/text_embedding_pipeline.py
+++ src/ml_resources/data/text_embedding_pipeline.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 embedding_length=384
 
-def apply_pipeline(data, columns):
+def apply_pipeline(data, columns, training=True):
     embedding_transformer = FunctionTransformer(lambda x: model.encode(x.iloc[:, 0].tolist()), validate=False)
     column_transformers=[]
     for column in columns:
@@ -21,13 +21,19 @@ def apply_pipeline(data, columns):
     )
     pipeline = Pipeline([("feature_creation", preprocessor)])
     transformed_data=list(pipeline.fit_transform(data))
+    if training:
+        topic_slice = [x[embedding_length*2+3] for x in transformed_data]
+    else:
+        topic_slice = None
+    print(transformed_data)
     transformed_embeddings = pd.DataFrame({"summary_embeddings": [x[0:embedding_length] for x in transformed_data],
      "category_embeddings": [x[embedding_length:embedding_length*2] for x in transformed_data],
      "item_type": [x[embedding_length*2] for x in transformed_data],
-     "topic": [x[embedding_length*2+1] for x in transformed_data],
-     "agency_id": [x[embedding_length*2+2] for x in transformed_data],
-     "has_categories": [x[embedding_length*2+3] for x in transformed_data]
+     "agency_id": [x[embedding_length*2+1] for x in transformed_data],
+     "has_categories": [x[embedding_length*2+2] for x in transformed_data]
      },
      index=data.index
-     )
+    )
+    if training:
+        transformed_embeddings["topic"] = topic_slice
     return transformed_embeddings
